f9a59376f17cfcb24c4fc0657031c794101ed320
Vinayak Joshi <vinayak.joshi@rummyverse.com>
2024-11-04 06:10:57 +0530

    core - added evn variable in server for API response delays

--- src/tester/server.ts
+++ src/tester/server.ts
@@ -2,13 +2,16 @@ import varients from "../varients";
 
 const input = process.argv[2];
 
+const API_RESPONSE_DELAY = +(process.env.API_RESPONSE_DELAY || 0);
+
 const allowedInputs = Object.keys(varients);
 if (!allowedInputs.includes(input)) {
     console.log(`Invalid input. Allowed inputs are: ${allowedInputs.join(', ')}`);
     process.exit(1);
 }
 varients[input].buildServer(async (request: any) => {
-    // console.log(`Server received request ${request?.requestId}`);
-    // await new Promise(resolve => setTimeout(resolve, 1));
+    if (API_RESPONSE_DELAY > 0) {
+        await new Promise(resolve => setTimeout(resolve, API_RESPONSE_DELAY));
+    }
     return request;
  });
